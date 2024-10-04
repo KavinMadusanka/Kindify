7e2a72f7c1c3c792cb9039b03d009b5f1244ae5a
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-04 15:31:05 +0530

    Create signupO.jsx
    Co-Authored-By: Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>

---
+++ client/app/login/signupO.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text,  Image, Pressable } from 'react-native'
+
+export default function signupO() {
+  return (
+    <View>
+      <Text>Sign O</Text>
+    </View>
+  )
+}
