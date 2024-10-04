be067043118c860006975678c3852bbdaec54a8e
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-04 15:28:19 +0530

    Update index.jsx
    Co-Authored-By: Navishka Gunawardana <128272519+navishkaG@users.noreply.github.com>

--- client/app/login/index.jsx
+++ client/app/login/index.jsx
@@ -1,9 +1,46 @@
-import { View, Text, Image, Pressable } from 'react-native'
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
-import { Link } from 'expo-router'
+import { View, Text, Image, TextInput, Button, Pressable } from 'react-native'
+import { SignedIn, SignedOut, useUser, useAuth, useSignIn  } from '@clerk/clerk-expo';
+import { Link, useRouter  } from 'expo-router'
 import React from 'react';
 
 export default function LoginScreen() {
+    const { user } = useUser()
+
+    const { isSignedIn } = useAuth()
+    const { signIn, setActive, isLoaded } = useSignIn()
+    const router = useRouter()
+
+    const [emailAddress, setEmailAddress] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    if (isSignedIn) {
+        return <Redirect href={'/'} />
+    }
+
+    const onSignInPress = React.useCallback(async () => {
+        if (!isLoaded) {
+          return
+        }
+    
+        try {
+          const signInAttempt = await signIn.create({
+            identifier: emailAddress,
+            password,
+          })
+    
+          if (signInAttempt.status === 'complete') {
+            await setActive({ session: signInAttempt.createdSessionId })
+            router.replace('/')
+          } else {
+            // See https://clerk.com/docs/custom-flows/error-handling
+            // for more info on error handling
+            console.error(JSON.stringify(signInAttempt, null, 2))
+          }
+        } catch (err) {
+          console.error(JSON.stringify(err, null, 2))
+        }
+      }, [isLoaded, emailAddress, password])
+
   return (
     <View style={{
         flex: 1,
@@ -19,26 +56,80 @@ export default function LoginScreen() {
                 style={{
                     width:200,
                     height:200,
-                    marginTop:150,
+                    marginTop:100,
                 }}
             />
         </View>
         <View style={{
             paddingTop:20,
-            alignItems: "center",
+            marginTop:30,
+            paddingLeft:15
             }}>
-            <Pressable style={{
+            <Text style={{
+                fontSize:20,
+                paddingLeft:15,
+                color:"#16423C"
+                }}>User Name :</Text>
+        </View>
+        <View style={{
+            paddingTop:10,
+            alignItems:"center"
+            }}>
+            <View style={{
                 padding:14,
-                marginTop:100,
-                backgroundColor:'gray',
+                backgroundColor:'#E9EFEC',
                 width:'90%',
-                borderRadius:14
+                color:"#16423C",
+                borderRadius:14}}>
+                <TextInput
+                    autoCapitalize="none"
+                    value={emailAddress}
+                    placeholder="Email..."
+                    style={{color:"#16423C"}}
+                    placeholderTextColor="#16423C"
+                    onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
+                />
+            </View>
+        </View>
+        <View style={{
+            paddingTop:20,
+            // marginTop:50,
+            paddingLeft:15
             }}>
-                <Text style={{
-                    fontSize:20,
-                    textAlign:'center',
-                }}>Get Started</Text>
-            </Pressable>
+            <Text style={{
+                fontSize:20,
+                paddingLeft:15,
+                color:"#16423C"
+                }}>Password :</Text>
+        </View>
+        <View style={{
+            paddingTop:10,
+            alignItems:"center"
+            }}>
+            <View style={{
+                padding:14,
+                backgroundColor:'#E9EFEC',
+                width:'90%',
+                color:"#16423C",
+                borderRadius:14}}>
+            <TextInput
+                value={password}
+                placeholder="Password..."
+                style={{color:"#16423C"}}
+                placeholderTextColor="#16423C"
+                secureTextEntry={true}
+                onChangeText={(password) => setPassword(password)}
+            />
+            </View>
+            <View style={{
+                padding:4,
+                marginTop:40,
+                backgroundColor:'#E9EFEC',
+                width:'40%',
+                color:"#16423C",
+                borderRadius:14}}>
+                <Button title="Log in" onPress={onSignInPress} />
+            </View>
         </View>
         <View>
             {/* <SignedIn>
@@ -48,10 +139,11 @@ export default function LoginScreen() {
                 <View style={{
                     fontSize:15,
                     paddingLeft:15,
-                    marginTop:150,
+                    marginTop:90,
+                    
                     }}>
                         <Link href="/login/select">
-                            <Text style={{fontFamily:'outfit-bold'}}>Create an Account ? Click Here.</Text>
+                            <Text style={{fontFamily:'outfit-bold',color:"#16423C"}}>Create an Account ? Click Here.</Text>
                         </Link>
                 </View>
                     
