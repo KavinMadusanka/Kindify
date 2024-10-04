cd9dd3bc72cbeb0cf145077b10e5c799babd1ed7
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-04 13:16:52 +0530

    Update _layout.jsx
    Co-Authored-By: Navishka Gunawardana <128272519+navishkaG@users.noreply.github.com>

--- client/app/_layout.jsx
+++ client/app/_layout.jsx
@@ -1,5 +1,32 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SecureStore from 'expo-secure-store'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      const item = await SecureStore.getItemAsync(key)
+      if (item) {
+        console.log(`${key} was used 🔐 \n`)
+      } else {
+        console.log('No values stored under key: ' + key)
+      }
+      return item
+    } catch (error) {
+      console.error('SecureStore get item error: ', error)
+      await SecureStore.deleteItemAsync(key)
+      return null
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value)
+    } catch (err) {
+      return
+    }
+  },
+}
 
 export default function RootLayout() {
 
@@ -8,17 +35,41 @@ export default function RootLayout() {
     'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
     'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf')
   })
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+  // if (!publishableKey) {
+  //   throw new Error(
+  //     'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  //   )
+  // }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" 
-      options={{
-        headerShown:false
-      }}/>
-      <Stack.Screen name="(tabs)" 
-      options={{
-        headerShown:false
-      }}/>
-    </Stack>
+    
+    <ClerkProvider 
+    tokenCache={tokenCache}
+    publishableKey={publishableKey}>
+      <Stack>
+        <Stack.Screen name="index" 
+        options={{
+          headerShown:false
+        }}/>
+        <Stack.Screen name="login/index" 
+        options={{
+          headerShown:false
+        }}/>
+        <Stack.Screen name="login/signup" 
+        options={{
+          headerShown:false
+        }}/>
+        <Stack.Screen name="login/select" 
+        options={{
+          headerShown:false
+        }}/>
+        <Stack.Screen name="(tabs)" 
+        options={{
+          headerShown:false
+        }}/>
+      </Stack>
+    </ClerkProvider>
   );
 }
