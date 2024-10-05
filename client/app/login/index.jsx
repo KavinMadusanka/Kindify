a684555061a0e8982b6870e85b583c566a7290cc
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-05 12:03:19 +0530

    Update index.jsx
    Co-Authored-By: Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>

--- client/app/login/index.jsx
+++ client/app/login/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TextInput, Button, Pressable } from 'react-native'
+import { View, Text, Image, TextInput, Button, Pressable, ScrollView, KeyboardAvoidingView, Platform  } from 'react-native'
 import { SignedIn, SignedOut, useUser, useAuth, useSignIn  } from '@clerk/clerk-expo';
 import { Link, useRouter  } from 'expo-router'
 import React from 'react';
@@ -30,7 +30,7 @@ export default function LoginScreen() {
     
           if (signInAttempt.status === 'complete') {
             await setActive({ session: signInAttempt.createdSessionId })
-            router.replace('/')
+            router.replace('(tabs)/home')
           } else {
             // See https://clerk.com/docs/custom-flows/error-handling
             // for more info on error handling
@@ -42,93 +42,100 @@ export default function LoginScreen() {
       }, [isLoaded, emailAddress, password])
 
   return (
+    <KeyboardAvoidingView
+        style={{ flex: 1, backgroundColor: "#C4DAD2" }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={30} // Adjust offset as needed
+    >
+        <ScrollView style={{ backgroundColor: "#C4DAD2", marginTop:50}}>
     <View style={{
         flex: 1,
         // justifyContent: "center",
         // alignItems: "center",
         backgroundColor:"#C4DAD2",
-    }}>
-        <View style={{
-            alignItems: "center",
-            width:'100%'
-        }}>
-            <Image source={require('./../../assets/images/Logo.png')}
-                style={{
-                    width:200,
-                    height:200,
-                    marginTop:100,
-                }}
-            />
-        </View>
-        <View style={{
-            paddingTop:20,
-            marginTop:30,
-            paddingLeft:15
-            }}>
-            <Text style={{
-                fontSize:20,
-                paddingLeft:15,
-                color:"#16423C"
-                }}>User Name :</Text>
-        </View>
-        <View style={{
-            paddingTop:10,
-            alignItems:"center"
-            }}>
+    }}><View>
             <View style={{
-                padding:14,
-                backgroundColor:'#E9EFEC',
-                width:'90%',
-                color:"#16423C",
-                borderRadius:14}}>
-                <TextInput
-                    autoCapitalize="none"
-                    value={emailAddress}
-                    placeholder="Email..."
-                    style={{color:"#16423C"}}
-                    placeholderTextColor="#16423C"
-                    onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
+                alignItems: "center",
+                width:'100%'
+            }}>
+                <Image source={require('./../../assets/images/Logo.png')}
+                    style={{
+                        width:200,
+                        height:200,
+                        marginTop:50,
+                    }}
                 />
             </View>
-        </View>
-        <View style={{
-            paddingTop:20,
-            // marginTop:50,
-            paddingLeft:15
-            }}>
-            <Text style={{
-                fontSize:20,
-                paddingLeft:15,
-                color:"#16423C"
-                }}>Password :</Text>
-        </View>
-        <View style={{
-            paddingTop:10,
-            alignItems:"center"
-            }}>
             <View style={{
-                padding:14,
-                backgroundColor:'#E9EFEC',
-                width:'90%',
-                color:"#16423C",
-                borderRadius:14}}>
-            <TextInput
-                value={password}
-                placeholder="Password..."
-                style={{color:"#16423C"}}
-                placeholderTextColor="#16423C"
-                secureTextEntry={true}
-                onChangeText={(password) => setPassword(password)}
-            />
+                paddingTop:20,
+                marginTop:30,
+                paddingLeft:15
+                }}>
+                <Text style={{
+                    fontSize:20,
+                    paddingLeft:15,
+                    color:"#16423C"
+                    }}>Email :</Text>
             </View>
             <View style={{
-                padding:4,
-                marginTop:40,
-                backgroundColor:'#E9EFEC',
-                width:'40%',
-                color:"#16423C",
-                borderRadius:14}}>
-                <Button title="Log in" onPress={onSignInPress} />
+                paddingTop:10,
+                alignItems:"center"
+                }}>
+                <View style={{
+                    padding:14,
+                    backgroundColor:'#E9EFEC',
+                    width:'90%',
+                    color:"#16423C",
+                    borderRadius:14}}>
+                    <TextInput
+                        autoCapitalize="none"
+                        value={emailAddress}
+                        placeholder="Email..."
+                        style={{color:"#16423C"}}
+                        placeholderTextColor="#16423C"
+                        onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
+                    />
+                </View>
+            </View>
+            <View style={{
+                paddingTop:20,
+                // marginTop:50,
+                paddingLeft:15
+                }}>
+                <Text style={{
+                    fontSize:20,
+                    paddingLeft:15,
+                    color:"#16423C"
+                    }}>Password :</Text>
+            </View>
+            <View style={{
+                paddingTop:10,
+                alignItems:"center"
+                }}>
+                <View style={{
+                    padding:14,
+                    backgroundColor:'#E9EFEC',
+                    width:'90%',
+                    color:"#16423C",
+                    borderRadius:14}}>
+                <TextInput
+                    value={password}
+                    placeholder="Password..."
+                    style={{color:"#16423C"}}
+                    placeholderTextColor="#16423C"
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                />
+                </View>
+                <View style={{
+                    padding:4,
+                    marginTop:40,
+                    backgroundColor:'#E9EFEC',
+                    width:'40%',
+                    color:"#16423C",
+                    borderRadius:14}}>
+                    <Button title="Log in" onPress={onSignInPress} color="#16423C"/>
+                </View>
             </View>
         </View>
         <View>
@@ -140,6 +147,7 @@ export default function LoginScreen() {
                     fontSize:15,
                     paddingLeft:15,
                     marginTop:90,
+                    marginBottom:80
                     
                     }}>
                         <Link href="/login/select">
@@ -153,5 +161,7 @@ export default function LoginScreen() {
             </SignedOut>
         </View>
     </View>
+    </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
