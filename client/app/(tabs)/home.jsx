8846515fbb0256466f6c0c50eb7fd835960226d6
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-06 11:12:57 +0530

    Update home.jsx
    Co-Authored-By: Piyusha Prabhasari <159011784+Prabhasari@users.noreply.github.com>

--- client/app/(tabs)/home.jsx
+++ client/app/(tabs)/home.jsx
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo';
 
 export default function home() {
+  const { signOut } = useAuth();  // Get the signOut method from Clerk
+
+  const handleLogout = async () => {
+    try {
+      await signOut();  // Call the signOut method to log out
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
   return (
     <View>
       <Text>home</Text>
+        <View>
+          <Button title="Log Out" onPress={handleLogout} />
+        </View>
     </View>
+    
   )
 }
