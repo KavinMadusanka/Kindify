961936a68f12c55dd3a5595e3d6e081accff713f
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-10-06 16:51:14 +0530

    Updated notification page
    Co-Authored-By: Navishka Gunawardana <128272519+navishkaG@users.noreply.github.com>
    Co-Authored-By: Piyusha Prabhasari <159011784+Prabhasari@users.noreply.github.com>
    Co-Authored-By: Kavin Madhusankha <163830061+KavinMadusanka@users.noreply.github.com>

--- client/app/(tabs)/notification.jsx
+++ client/app/(tabs)/notification.jsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
-export default function notification() {
+export default function notification() {  // It's a good practice to use PascalCase for component names
   return (
-    <View>
-      <Text>notification</Text>
+    <View style={styles.container}>
+      <Text>Notifications</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D5E5E4', // background color
+    padding: 20,
+    justifyContent: 'center', // Center the content vertically
+    alignItems: 'center',     // Center the content horizontally
+  },
+});
