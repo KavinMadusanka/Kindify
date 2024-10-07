a1673535c36ddbef2c67597711e14d6f451e9090
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-07 16:00:25 +0530

    Update profile.jsx

--- client/app/(Otabs)/profile.jsx
+++ client/app/(Otabs)/profile.jsx
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useAuth, useUser } from '@clerk/clerk-expo';
 
 export default function profile() {
+
+  const { signOut } = useAuth();  // Get the signOut method from Clerk
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    try {
+      await signOut();  // Call the signOut method to log out
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={{paddingTop:50}}>
+      <Text>Profile</Text>
+        <View style={{paddingTop:50}}>
+          <Button title="Log Out" onPress={handleLogout} />
+          {/* <Text>{user?.firstname}</Text>  */}
+        </View>
     </View>
   )
 }
