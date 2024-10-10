c86eb4441b55f5548bebe2087391d0c1d59f1a11
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-10 11:19:56 +0530

    database change to serendib

--- client/config/FirebaseConfig.js
+++ client/config/FirebaseConfig.js
@@ -5,23 +5,23 @@ import { getFirestore }from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY, 
-//   authDomain: "kindify-caa09.firebaseapp.com",
-//   projectId: "kindify-caa09",
-//   storageBucket: "kindify-caa09.appspot.com",
-//   messagingSenderId: "290288568723",
-//   appId: "1:290288568723:web:324596c5ea06036cfb4066"
-// };
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "kindify-9ba49.firebaseapp.com",
-  projectId: "kindify-9ba49",
-  storageBucket: "kindify-9ba49.appspot.com",
-  messagingSenderId: "878347969450",
-  appId: "1:878347969450:web:e0c63fa5a9c8fea9f3c1d3",
-  measurementId: "G-4Y9XM91CR9"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY, 
+  authDomain: "kindify-caa09.firebaseapp.com",
+  projectId: "kindify-caa09",
+  storageBucket: "kindify-caa09.appspot.com",
+  messagingSenderId: "290288568723",
+  appId: "1:290288568723:web:324596c5ea06036cfb4066"
 };
+// const firebaseConfig = {
+//   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: "kindify-9ba49.firebaseapp.com",
+//   projectId: "kindify-9ba49",
+//   storageBucket: "kindify-9ba49.appspot.com",
+//   messagingSenderId: "878347969450",
+//   appId: "1:878347969450:web:e0c63fa5a9c8fea9f3c1d3",
+//   measurementId: "G-4Y9XM91CR9"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
